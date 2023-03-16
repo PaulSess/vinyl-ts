702e5b0f915edd6a88d8b997fb02b4231412a14a
Paul Sessbruegger <paul.at.home@gmx.de>
2023-03-16 22:01:25 +0100

    video big

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 
+
 import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -25,10 +26,14 @@ export default function Home() {
         </nav>
       </section>
       <iframe
+        id='herovideo'
         src="https://drive.google.com/file/d/1fiOlOy5wMR5uA2CpYpf0xXbPv44XzLX4/preview"
         width="100%" height="100%" //allow="autoplay"
         style={{position:'absolute', top:'0'}}>
       </iframe>
+      {/* <script type="text/javascript">
+        const herovideo = document.getElementById('herovideo');
+      </script> */}
     </>
   )
 }
