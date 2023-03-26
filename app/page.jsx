a050382c90d4639eca38723237a8cf7c2d2fa143
Paul Sessbruegger <paul.at.home@gmx.de>
2023-03-26 22:56:31 +0200

    fonts added

--- app/page.jsx
+++ app/page.jsx
@@ -15,6 +15,12 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Gloock&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;1,500;1,700;1,900&family=Taviraj:ital,wght@0,200;0,300;0,400;0,600;1,100;1,200;1,300;1,400&display=swap" rel="stylesheet" />
+      </Head>
+
       <Navbar />
       <Herovideo />
 
