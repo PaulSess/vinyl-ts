d9ae6586a682770321029053d60000d3b1b1c6ff
Paul Sessbruegger <paul.at.home@gmx.de>
2023-03-27 17:14:06 +0200

    fixed navbar, hero video was blocking it, also navbar links are in next.js convention written now

--- app/components/herovideo.jsx
+++ app/components/herovideo.jsx
@@ -2,24 +2,27 @@ import herovideo from "../stylesheets/herovideo.css"
 
 export default function Herovideo() {
   //line underneath is needed for using document.   Should do the autoplaying, doesn't work tho
-  if (typeof window === 'object') {
-    window.onload = function() {
-      document.getElementById("herovideo").play();
-      }
-  }
+  // if (typeof window === 'object') {
+  //   window.onload = function() {
+  //     document.getElementById("herovideo").play();
+  //     }
+  // }
   return (
     // <iframe
     //   id='herovideo'
     //   src="https://drive.google.com/file/d/1fiOlOy5wMR5uA2CpYpf0xXbPv44XzLX4/preview"
     //   allow="autoplay"
     //   autoplay="autoplay" />
-    <iframe
+    <>
+     {/* <iframe
       id='herovideo'
       src="#"
       title="YouTube video player"
       frameborder="0"
       autoplay="autoplay"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen />
+      allowfullscreen
+    /> */}
+    </>
   )
 }
